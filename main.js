ef236468d46668cb3f754095c3ccaee2ab39636b
tade18 <tade.18@seznam.cz>
2023-10-25 10:33:35 +0200

    update3

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const objekt = document.getElementById("objekt")
 const startButton = document.getElementById("startButton")
-
+let gameInterval;
 startButton.onclick =()=>{
-    movePoint(objekt, getRandomNumber(80,600), getRandomNumber(80,600));
+    hideElement(startButton);
+    startGameInterval(objekt);
+
 }
 const movePoint = (element, x, y) => {
     element.style.top=`${y}px`;
@@ -10,3 +12,14 @@ const movePoint = (element, x, y) => {
 }
 
 const getRandomNumber =(minimum,maximum)=> Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+const hideElement = (element) => {
+    element.style.display = "none";
+}
+
+const startGameInterval = (element) => {
+    clearInterval(gameInterval);
+    gameInterval=setInterval(()=>{
+        movePoint(element, getRandomNumber(80,600), getRandomNumber(80,600));
+    },550)
+}
