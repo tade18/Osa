571c20277b91254c3ab8c222daed195c2ea0cc1c
tade18 <tade.18@seznam.cz>
2023-10-25 10:21:19 +0200

    update 2

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const objekt = document.getElementById("objekt")
 const startButton = document.getElementById("startButton")
 
 startButton.onclick =()=>{
-    movePoint(objekt, 100, 400);
+    movePoint(objekt, getRandomNumber(80,600), getRandomNumber(80,600));
 }
 const movePoint = (element, x, y) => {
     element.style.top=`${y}px`;
     element.style.left=`${x}px`;
 }
+
+const getRandomNumber =(minimum,maximum)=> Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
