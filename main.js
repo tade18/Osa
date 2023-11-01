a1f31946df9e4d7760a3791b343844d13488e885
tade18 <tade.18@seznam.cz>
2023-11-01 10:13:19 +0100

    update 0111123

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const objekt = document.getElementById("objekt")
-const startButton = document.getElementById("startButton")
+const objekt = document.getElementById("objekt");
+const startButton = document.getElementById("startButton");
+const timer = document.getElementById("timer");
 let gameInterval;
+let timeStart;
 startButton.onclick =()=>{
     hideElement(startButton);
     startGameInterval(objekt);
-
+    setPointClick(objekt);
+    timeStart = performance.now();
 }
 const movePoint = (element, x, y) => {
     element.style.top=`${y}px`;
@@ -20,6 +23,23 @@ const hideElement = (element) => {
 const startGameInterval = (element) => {
     clearInterval(gameInterval);
     gameInterval=setInterval(()=>{
-        movePoint(element, getRandomNumber(80,600), getRandomNumber(80,600));
+        movePoint(element, getRandomNumber(80,window.innerWidth-parseInt(element.style.width)), getRandomNumber(80,window.innerHeight-parseInt(element.style.height)));
+        setSize(element, getRandomNumber(40, 80));
     },550)
+}
+
+const setPointClick = (element) => {
+    element.onclick = () => {
+        element.innerText++;
+        movePoint(element, getRandomNumber(80,window.innerWidth-parseInt(element.style.width)), getRandomNumber(80,window.innerHeight-parseInt.style.height));
+        setSize(element, getRandomNumber(40, 80));
+        let timeEnd = performance.now();
+        let result = timeEnd - timeStart;
+        timer.innerText = `Time: ${result}ms`;
+    }
+}
+
+const setSize = (element, size) => {
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
 }
